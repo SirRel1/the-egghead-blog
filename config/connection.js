e25b4e63306d82cab469723a4a55d57a130f5fd6
Terrell Hudson <thudson5731@gmail.com>
2022-02-25 00:19:43 -0500

    changing connection variables for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URL);
+
+
+
+let sequelize; 
 
 const URI = process.env.MYSQL_URL || process.env.JAWSDB_URL 
+
+sequelize = new Sequelize(URI)
+
+
+
+module.exports = sequelize;
 
 
 module.exports = sequelize;
